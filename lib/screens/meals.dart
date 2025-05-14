3b176228269c5cb4b3a7f54572653e87f223e346
Janine-Codes <janinekrse@gmail.com>
2025-05-14 13:52:05 +0200

    Pushing project

---
+++ lib/screens/meals.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/screens/meal_details.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({
+    super.key,
+    this.title,
+    required this.meals,
+  });
+
+  final String? title;
+  final List<Meal> meals;
+
+  void selectMeal(BuildContext context, Meal meal) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealDetailsScreen(
+          meal: meal,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (ctx, index) => MealItem(
+        meal: meals[index],
+        onSelectMeal: selectMeal,
+      ),
+    );
+
+    if (title == null) {
+      return content;
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title!),
+        centerTitle: false,
+      ),
+      body: content,
+    );
+  }
+}
